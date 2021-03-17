5efb2dddac6da6bec4dddb3463522922e3ba3f07
Neeraj Singh <1378886+neerajsinghsonu@users.noreply.github.com>
2021-03-18 03:10:02 +0530

    commit from dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PrerenderComponent } from './prerender/prerender.component';
+import { SsrComponent } from './ssr/ssr.component';
+import { SsgComponent } from './ssg/ssg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    PrerenderComponent,
+    SsrComponent,
+    SsgComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule
   ],
   providers: [],
